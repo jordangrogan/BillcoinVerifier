45140702a52f210a92d68bb1b7867eebbe094e44
Johnfcs778 <johnfahnestock@live.com>
2018-02-22 21:08:18 -0500

    added some more code to split up the lines

--- verifier.rb
+++ verifier.rb
@@ -1,12 +1,12 @@
+# method to verify block numbers are in order
 def verifyLineOrder lineNums
   oldVal = -1
   lineNums.each do |lineInt|
     if lineInt.to_i < oldVal
-      puts "false"
       return false
     end
+    oldVal = lineInt.to_i
   end
-  puts "true"
   return true
 end
 
@@ -14,14 +14,31 @@ end
 class Verifier
   # var to keep track of the line numbers for each block
   @lineNums = []
+  @transactionData = []
   # main loop going through each line of file
   File.open("sample.txt").each do |line|
     currLine =  line.to_s
+    @transactionData.push(currLine.split("|"))
     @lineNums.push(currLine[0])
   end
 
-  # method to verify block numbers are in order
-  verifyLineOrder(@lineNums)
+  # This currently separates the actual transaction data portion of the line
+  # so that the totals for each person can be calculated 
+  @transactionData.each do |splitData|
+    count = 1
+    splitData.each do |splitLine|
+      if(count == 3)
+        puts splitLine
+      end
+      count +=1;
+    end
+  end
+
+  if verifyLineOrder(@lineNums)
+    puts "block line check passed"
+  else
+    puts "block line check failed"
+  end
 end
 
 

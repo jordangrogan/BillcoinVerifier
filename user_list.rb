d398a394761a86643e735445a9a6c14feed69e06
jfahnestock <johnfahnestock@live.com>
2018-02-23 15:44:37 -0500

    Modified user_list to use a cache to lookup user existence O(n)->O(1)

--- user_list.rb
+++ user_list.rb
@@ -2,20 +2,22 @@ require_relative "User"
 # This class will hold the master list of users on the Billcoin blockchain
 class UserList
   def initialize
-    @userList = []
+    @user_list = []
+    @user_cache = {} # create new hashmap to hold name cache
   end
 
   def addUser user
-    @userList.push(user)
+    @user_list.push(user)
+    @user_cache[user] = 1
   end
 
   # This method will return whether a use exists in the system
+  # Uses user cache to get a constant lookup time
   def userExist? username
-    @userList.each do |user|
-      if user.name.eql? (username)
-        return true
-      end
+    if @user_cache.has_key? username
+      return true
     end
     return false
   end
+
 end

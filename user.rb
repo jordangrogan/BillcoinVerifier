449a0d2025e32ff1bafc2b555bcb6582653507a2
Jordan Grogan <jordangrogan11@gmail.com>
2018-02-24 18:23:18 -0500

    Updated attr_accessor line to correct symbol names

--- user.rb
+++ user.rb
@@ -1,7 +1,7 @@
 # This class will represent a user in the Billcoin blockchain
 # The user will have a name and a number of Billcoins associated with them
 class User
-  attr_accessor :@numCoins, :@name
+  attr_accessor :numCoins, :name
   def initialize name, start_amt
     @name = name
     @num_coins = start_amt

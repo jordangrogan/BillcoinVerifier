07f13a967590756132ce0b68ba23d7f15508a129
Jordan Grogan <jordangrogan11@gmail.com>
2018-02-24 21:51:47 -0500

    Refactored code into testable methods and created test file for BillcoinVerifier

---
+++ billcoin_verifier.rb
@@ -0,0 +1,124 @@
+require_relative "block"
+require_relative "user_list"
+
+class BillcoinVerifier
+
+  attr_accessor :blocks
+
+  @user_list = UserList::new
+  @blocks = []
+
+  def open_file(input_file)
+
+    File.open(input_file, "r").each_line do |line|
+      block_split = line.chomp.split("|")
+      block = Block::new(block_split[0], block_split[1], block_split[2], block_split[3], block_split[4])
+      @blocks << block
+    end
+
+  end
+
+  def run_verifications
+
+    gather_transaction_data
+
+    if !verify_line_order
+      puts "BLOCKCHAIN INVALID"
+    end
+
+    if !verify_hash
+      puts "BLOCKCHAIN INVALID"
+    end
+
+  end
+
+  # This currently separates the actual transaction data portion of the line
+  # so that the totals for each person can be calculated
+  def gather_transaction_data
+
+    @blocks.each do |block|
+
+      splitLine = block.transactions
+
+      loop do
+        stripstring = splitLine[/[A-Z]*[a-z]*(>)[A-Z]*[a-z]*[\(][0-9]*[\)][\:]*/]
+
+        unless stripstring.eql? nil
+          #puts " #{t_from(stripstring)} is giving #{t_to(stripstring)} #{t_amt(stripstring)} billcoins "
+          # ready to start populating here
+          splitLine = splitLine[stripstring.length-1,splitLine.length]
+        end
+
+        break if stripstring.eql? nil
+      end
+
+    end
+
+  end
+
+  # This function takes a transaction string in the format of USER1>USER2(NUMBILLCOINS)
+  # This function will return the name of USER1, the USER sending billcoins
+  def t_from t_string
+    first = t_string[/[A-Z]*[a-z]*(>)/]
+    return first.chop
+  end
+  # This function takes a transaction string in the format of USER1>USER2(NUMBILLCOINS)
+  # This function will return the name of USER2, the USER receiving billcoins
+  def t_to t_string
+    second = t_string[/(>)[A-Z]*[a-z]*/]
+    return second[1,second.length]
+  end
+  # This function takes a transaction string in the format of USER1>USER2(NUMBILLCOINS)
+  # This function will return the amount of billcoins transfered in this transaction
+  def t_amt t_string
+    amt = t_string[/[\(][0-9]*[\)]/]
+    return amt[1,amt.length-2]
+  end
+
+  # method to verify block numbers are in order
+  def verify_line_order
+    i = 0
+    @blocks.each do |block|
+      line_num = block.number
+      if line_num.to_i != i
+        puts "Line #{i}: Invalid block number #{line_num}, should be #{i}"
+        return false
+      end
+      i += 1
+    end
+    true
+  end
+
+  def verify_hash
+    hashed_string = "0"
+    @blocks.each do |block|
+
+      # Check previous hash
+      if hashed_string != block.hash_prev_block
+        puts "Line #{block.number}: Previous hash was #{block.hash_prev_block}, should be #{hashed_string}"
+        return false
+      end
+
+      # Check this block's hash
+      interpolated_string = "#{block.number}|#{block.hash_prev_block}|#{block.transactions}|#{block.timestamp}"
+      hashed_string = hash(interpolated_string)
+      if block.hash_this_block != hashed_string
+        puts "Line #{block.number}: String '#{interpolated_string}' hash set to #{block.hash_this_block}, should be #{hashed_string}"
+        return false
+      end
+
+      true
+    end
+  end
+
+  def hash string
+    val = 0
+    utf_8_characters = string.unpack('U*')
+    utf_8_characters.each do |x|
+      val += (x ** 2000) * ((x + 2) ** 21) - ((x + 5) ** 3)
+    end
+    val %= 65536
+    val.to_s(16)
+  end
+
+end
---
+++ billcoin_verifier_test.rb
@@ -0,0 +1,24 @@
+require 'minitest/autorun'
+
+require_relative 'billcoin_verifier'
+
+class BillcoinVerifierTest < Minitest::Test
+
+  def setup
+    @v = BillcoinVerifier::new()
+  end
+
+  def test_verify_line_order_in_order
+    block0 = Minitest::Mock::new("block0")
+    def block0.number; 0; end
+    block1 = Minitest::Mock::new("block1")
+    def block1.number; 1; end
+    block2 = Minitest::Mock::new("block2")
+    def block2.number; 2; end
+    block3 = Minitest::Mock::new("block3")
+    def block3.number; 3; end
+    @v.blocks = [block0, block1, block2, block3]
+    assert_equal @v.verify_line_order, true
+  end
+
+end

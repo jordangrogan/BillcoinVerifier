972e9531eea1caad0de41ce44f1771ff680835ba
Jordan Grogan <jordangrogan11@gmail.com>
2018-02-24 21:57:31 -0500

    Verified out of order lines

--- billcoin_verifier_test.rb
+++ billcoin_verifier_test.rb
@@ -21,4 +21,17 @@ class BillcoinVerifierTest < Minitest::Test
     assert_equal @v.verify_line_order, true
   end
 
+  def test_verify_line_order_out_of_order
+    block0 = Minitest::Mock::new("block0")
+    def block0.number; 0; end
+    block1 = Minitest::Mock::new("block1")
+    def block1.number; 1; end
+    block2 = Minitest::Mock::new("block2")
+    def block2.number; 3; end
+    block3 = Minitest::Mock::new("block3")
+    def block3.number; 2; end
+    @v.blocks = [block0, block1, block2, block3]
+    assert_output("Line 2: Invalid block number 3, should be 2\n") { @v.verify_line_order }
+  end
+
 end

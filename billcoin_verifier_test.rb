eec57f80c8951e6a4367bc6f9c71091345a3eef2
Jordan Grogan <jordangrogan11@gmail.com>
2018-02-28 23:57:47 -0500

    More tests for verify_hash

--- billcoin_verifier_test.rb
+++ billcoin_verifier_test.rb
@@ -39,7 +39,7 @@ class BillcoinVerifierTest < Minitest::Test
     assert_equal @v.hash("1|1c12|SYSTEM>George(100)|1518892051.740967000"), "abb2"
   end
 
-  # TODO: Should probably sprout out the verify_hash method so that there's a interpolate_string method, maybe more, then this test wouldn't be so long
+  # This uses the first 4 blocks from sample.txt, which we know is correct
   def test_verify_hash
     block0 = Minitest::Mock::new("block0")
     def block0.number; 0; end
@@ -67,6 +67,40 @@ class BillcoinVerifierTest < Minitest::Test
     def block3.hash_this_block; "7419"; end
     @v.blocks = [block0, block1, block2, block3]
     assert @v.verify_hash
+  end
+
+  def test_verify_hash_prev_hash_bad
+    block0 = Minitest::Mock::new("block0")
+    def block0.number; 0; end
+    def block0.hash_prev_block; "0"; end
+    def block0.transactions; "SYSTEM>Henry(100)"; end
+    def block0.timestamp; "1518892051.737141000"; end
+    def block0.hash_this_block; "1c12"; end
+    block1 = Minitest::Mock::new("block1")
+    def block1.number; 1; end
+    def block1.hash_prev_block; "1c12"; end
+    def block1.transactions; "SYSTEM>George(100)"; end
+    def block1.timestamp; "1518892051.740967000"; end
+    def block1.hash_this_block; "abb2"; end
+    block2 = Minitest::Mock::new("block2")
+    def block2.number; 2; end
+    def block2.hash_prev_block; "abb3"; end # BAD!
+    def block2.transactions; "George>Amina(16):Henry>James(4):Henry>Cyrus(17):Henry>Kublai(4):George>Rana(1):SYSTEM>Wu(100)"; end
+    def block2.timestamp; "1518892051.753197000"; end
+    def block2.hash_this_block; "c72d"; end
+    @v.blocks = [block0, block1, block2]
+    refute @v.verify_hash
+  end
+
+  def test_verify_hash_this_hash_bad
+    block0 = Minitest::Mock::new("block0")
+    def block0.number; 0; end
+    def block0.hash_prev_block; "0"; end
+    def block0.transactions; "SYSTEM>Henry(100)"; end
+    def block0.timestamp; "1518892051.737141000"; end
+    def block0.hash_this_block; "1c13"; end # BAD!
+    @v.blocks = [block0]
+    refute @v.verify_hash
   end
 
 end

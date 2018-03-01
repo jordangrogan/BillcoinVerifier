642efeb0c3f59552e74e8f709108bdf62f4b1b95
Jordan Grogan <jordangrogan11@gmail.com>
2018-03-01 00:34:10 -0500

    Modifies tests for verify_hash and adds comments

--- billcoin_verifier_test.rb
+++ billcoin_verifier_test.rb
@@ -8,6 +8,11 @@ class BillcoinVerifierTest < Minitest::Test
     @v = BillcoinVerifier::new()
   end
 
+  # UNIT TESTS FOR verify_line_order
+  # Blocks in order     --> returns true
+  # Blocks out of order --> returns false & outputs issue
+
+  # Doubles bBocks & verifies that the blocks are in order
   def test_verify_line_order_in_order
     block0 = Minitest::Mock::new("block0")
     def block0.number; 0; end
@@ -21,6 +26,8 @@ class BillcoinVerifierTest < Minitest::Test
     assert @v.verify_line_order
   end
 
+  # Doubles Blocks & puts them out of order to make sure that the
+  # correct string is outputted & returns false
   def test_verify_line_order_out_of_order
     block0 = Minitest::Mock::new("block0")
     def block0.number; 0; end
@@ -32,12 +39,19 @@ class BillcoinVerifierTest < Minitest::Test
     def block3.number; 2; end
     @v.blocks = [block0, block1, block2, block3]
     assert_output("Line 2: Invalid block number 3, should be 2\n") { @v.verify_line_order }
+    refute @v.verify_line_order
   end
 
+  # UNIT TEST FOR hash
+  # This test tests two strings to make sure the correct hash is returned
   def test_hash
     assert_equal @v.hash("0|0|SYSTEM>Henry(100)|1518892051.737141000"), "1c12"
     assert_equal @v.hash("1|1c12|SYSTEM>George(100)|1518892051.740967000"), "abb2"
   end
+
+  # UNIT TESTS FOR verify_hash
+  # On success --> returns true
+  # On failure --> outputs error & returns false
 
   # This uses the first 4 blocks from sample.txt, which we know is correct
   def test_verify_hash
@@ -69,6 +83,7 @@ class BillcoinVerifierTest < Minitest::Test
     assert @v.verify_hash
   end
 
+  # This tests when the previous hash is bad (returns false & outputs issue)
   def test_verify_hash_prev_hash_bad
     block0 = Minitest::Mock::new("block0")
     def block0.number; 0; end
@@ -89,9 +104,11 @@ class BillcoinVerifierTest < Minitest::Test
     def block2.timestamp; "1518892051.753197000"; end
     def block2.hash_this_block; "c72d"; end
     @v.blocks = [block0, block1, block2]
+    assert_output("Line 2: Previous hash was abb3, should be abb2\n") { @v.verify_hash }
     refute @v.verify_hash
   end
 
+  # This tests when the current block's hash is bad (returns false & outputs issue)
   def test_verify_hash_this_hash_bad
     block0 = Minitest::Mock::new("block0")
     def block0.number; 0; end
@@ -100,6 +117,7 @@ class BillcoinVerifierTest < Minitest::Test
     def block0.timestamp; "1518892051.737141000"; end
     def block0.hash_this_block; "1c13"; end # BAD!
     @v.blocks = [block0]
+    assert_output("Line 0: String '0|0|SYSTEM>Henry(100)|1518892051.737141000' hash set to 1c13, should be 1c12\n") { @v.verify_hash }
     refute @v.verify_hash
   end
 

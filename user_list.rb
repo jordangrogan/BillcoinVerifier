bc677cb8fb0899a362fca2cfe8510bee10d9f8a1
Jordan Grogan <jordangrogan11@gmail.com>
2018-03-11 16:45:11 -0400

    Use hashmap for @user_list

--- user_list.rb
+++ user_list.rb
@@ -1,33 +1,32 @@
 require_relative "User"
+
 # This class will hold the master list of users on the Billcoin blockchain
 class UserList
   def initialize
-    @user_list = []
-    @user_cache = {} # create new hashmap to hold name cache
+    @user_list = {}
   end
 
   def add_user user, start
     new_user = User.new(user,start)
-    @user_list << new_user
-    @user_cache[user] = @user_list.size - 1 # the value is the index in the @user_list array
+    @user_list[user] = new_user
     new_user
   end
 
   # This method will return whether a use exists in the system
   # Uses user cache to get a constant lookup time
   def user_exist? username
-    if @user_cache.has_key? username
+    if @user_list.has_key? username
       return true
     end
-    return false
+    false
   end
 
   def find_user username
-    @user_list[@user_cache[username]]
+    @user_list[username]
   end
 
   def output_transaction_data
-    @user_list.each do |user|
+    @user_list.each do |key, user|
       puts "#{user.name}: #{user.num_coins} billcoins"
     end
   end
